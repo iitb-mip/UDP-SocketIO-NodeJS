4729186206e047fd5c81c0c277e311b0d0d609e0
Shashank Padwal <shashank23padwal@live.com>
2020-07-31 03:02:01 +0530

    minor changes

--- server.js
+++ server.js
@@ -19,20 +19,17 @@ io.on("connection", (socket) => {
 function doStuff() {}
 
 http.listen(3000, () => {
-  console.log("listening on *:3000");
+  console.log("http server on : 3000");
 });
 
 server.on("listening", function () {
   var address = server.address();
-  console.log(
-    "UDP Server listening on " + address.address + ":" + address.port
-  );
+  console.log("UDP Server listening on :" + address.port);
 });
 
 server.on("message", function (message, remote) {
   message = message.toString();
   io.sockets.emit("message", message);
 });
-// echo "hi" | nc -w1 -u 127.0.0.1 5000
 
 server.bind(5000);
